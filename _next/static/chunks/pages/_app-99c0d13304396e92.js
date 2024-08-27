e1a649185e444d9d56f003a29a533d4e18a7dc9e
zelevar <zelevarx@proton.me>
2024-08-27 19:58:48 +0300

    Deploy Next.js build

---
+++ _next/static/chunks/pages/_app-99c0d13304396e92.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{3990:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(113)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(3990),_(9848)}),_N_E=n.O()}]);
